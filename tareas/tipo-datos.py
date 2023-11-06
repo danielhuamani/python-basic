c79e19abe3b864670ca00cabe9e9769cf5ea6e20
daniel huamani <danielhuamani15@gmail.com>
2023-11-06 18:19:11 -0500

    added module tareas

---
+++ tareas/tipo-datos.py
@@ -0,0 +1,54 @@
+'''
+    Tarea sobre cadena de texto (string), para realizar la tarea usar la funcion "print".
+    Usar como referencia estos links:
+    https://blog.soyhenry.com/metodos-no-tan-vistos-en-python/
+    https://programacionfacil.org/blog/los-metodos-de-string-upper-lower-capitalize-y-title-de-python/
+'''
+
+
+'''
+    convertir la cadena de texto en  capital letter.
+    ejemplo:
+        bienvenido a programar
+    resultado esperado:
+        Bienvenido a programar
+'''
+
+variable_to_capitalletter = "bienvenido a programar"
+
+# print -> resultado
+
+'''
+    convertir la cadena de texto en todo mayuscula.
+    ejemplo:
+        primera clase
+    resultado esperado:
+        PRIMERA CLASE
+'''
+
+variable_lower_to_upper = "primera clase"
+
+
+# print -> resultado
+
+'''
+    convertir la cadena de texto en todo miniscula.
+    ejemplo:
+        APRENDIENDO PYTHON
+    resultado esperado:
+        aprendiendo python
+'''
+
+variable_upper_to_lower = "APRENDIENDO PYTHON"
+
+
+
+'''
+    convertir la cadena de texto en tipo titulo.
+    ejemplo:
+        conociendo el lenguaje de programacion python
+    resultado esperado:
+        Conociendo El Lenguaje De Programacion Python
+'''
+
+variable_to_title = "conociendo el lenguaje de programacion python"
