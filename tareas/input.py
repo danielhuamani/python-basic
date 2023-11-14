d2543cec24c475bec83e64d511dc9ecdf015d919
daniel huamani <danielhuamani15@gmail.com>
2023-11-14 13:10:34 -0500

    remove test

--- tareas/input.py
+++ tareas/input.py
@@ -56,11 +56,3 @@
 
 '''
 # escribir codigo aquí
-
-
-'''
-    Tarea 6: Crea un programa que calcule el área de un rectángulo. 
-    El usuario debe ingresar la longitud y el ancho del rectángulo.
-
-'''
-# escribir codigo aquí
