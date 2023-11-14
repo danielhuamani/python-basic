ecd2782e865b2e4280d9d4141734d0a23c15aa97
daniel huamani <danielhuamani15@gmail.com>
2023-11-14 10:44:30 -0500

    added tarea

--- tareas/input.py
+++ tareas/input.py
@@ -1,14 +1,37 @@
 '''
     Ejercicios de Input.
     Puedes leer documentacion de:
-    input -> https://www.programarya.com/Cursos/Python/entrada-y-lectura-de-datos
-             https://www.mclibre.org/consultar/python/lecciones/python-entrada-teclado.html
-    formateo de string -> https://www.freecodecamp.org/espanol/news/formato-de-cadenas-en-python-ejemplos-de-formato-s-sprint-en-python/
+    cast(conversion de datos):
+        - https://www.freecodecamp.org/espanol/news/como-convertir-strings-en-enteros-en-python/
+        - https://www.programacionfacil.org/cursos/python_avanzado/python_avanzado_3_tipos_de_datos_y_conversiones.html
+    input (ingreso de datos mediante la terminal):
+        - https://www.programarya.com/Cursos/Python/entrada-y-lectura-de-datos
+        - https://www.mclibre.org/consultar/python/lecciones/python-entrada-teclado.html
+    formateo de string: 
+        - https://www.freecodecamp.org/espanol/news/formato-de-cadenas-en-python-ejemplos-de-formato-s-sprint-en-python/
+        - https://www.freecodecamp.org/espanol/news/tutorial-de-f-strings-en-python-formato-de-cadenas-en-python-explicado-con-ejemplos/
     Para probar tu codigo puedes ejecutar el programa con 'python3 tareas/input.py'
 '''
 
+
 '''
-    Tarea 1: Escribe un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora.
+    Tarea 1: Desarrolla un programa que solicite al usuario que ingrese un número en forma de cadena, 
+    luego conviértelo a entero (int) y flotante (float) e imprime ambas conversiones.
+'''
+
+# escribir codigo aquí
+
+
+'''
+    Tarea 2: Escribe un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora.
+            Después debe mostrar por pantalla la paga que le corresponde.
+    recordar paga total es: nro horas trabajaas * coste por hora
+'''
+
+# escribir codigo aquí
+
+'''
+    Tarea 3: Escribe un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora.
             Después debe mostrar por pantalla la paga que le corresponde.
     recordar paga total es: nro horas trabajaas * coste por hora
 '''
@@ -17,18 +40,10 @@
 
 
 '''
-    Tarea 2: Crea un programa que ingrese por teclado dos valores, de como resultado la suma de estos dos valores.
+    Tarea 4: Crea un programa que ingrese por teclado dos valores, de como resultado la suma de estos dos valores.
 
 '''
 
-# escribir codigo aquí
-
-
-'''
-    Tarea 3: Crea un programa que calcule el área de un rectángulo. 
-    El usuario debe ingresar la longitud y el ancho del rectángulo.
-
-'''
 # escribir codigo aquí
 
 
@@ -37,3 +52,12 @@
     El usuario debe ingresar la longitud y el ancho del rectángulo.
 
 '''
+# escribir codigo aquí
+
+
+'''
+    Tarea 6: Crea un programa que calcule el área de un rectángulo. 
+    El usuario debe ingresar la longitud y el ancho del rectángulo.
+
+'''
+# escribir codigo aquí
