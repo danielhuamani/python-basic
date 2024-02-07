bc0a8be7d1d84c183f590538115fd350a626e73e
daniel huamani <danielhuamani15@gmail.com>
2024-02-07 17:57:36 -0500

    agrego tipo de datos diccionario

--- tipos_datos.py
+++ tipos_datos.py
@@ -20,12 +20,13 @@ variable = 5
 variable = "hola"
 geral = 5
 casa = 5
-casa_grande = 5
+casa_grande = 5 # 
 _casa = 5
 variable10 = 5
 variaBle = 5
 x,y,z = 2,4,5
-
+PI = 3.14
+SEGUNDOS_EN_MINUTO = 60
 # Nombers invalidos de varible
 # 3variable = 5
 # variable-casa=5
@@ -43,20 +44,20 @@ x,y,z = 2,4,5
     is = identificar tipo instancia
     type -> sirve para saber que tipo de dato es
 '''
-#  TIPOS DE DATOS NUMERICO ENTERO Y FLOTANTES
+#  TIPOS DE DATOS NUMERICO-> ENTERO Y FLOTANTES
 '''
     Entero -> 1 , 0 , -1 (int)
 '''
 
-varible1 = 1
-varible2 = 2
+variable1 = 1
+variable2 = 2
 
 '''Float (float)'''
 x = 2.4
 y = 3.7
 
-# print('tipo_variable1:', type(varible1))
-# print('tipo de dato de x:', type(x))
+print('tipo de dato de variable1:', type(variable1))
+print('tipo de dato de x:', type(x))
 
 
 '''TIPO DE DATO STRING (CADENA DE TEXTO) -> str'''
@@ -64,17 +65,19 @@ y = 3.7
 x = 'buenos dias "geral" '
 y = "buenos dias"
 z = "Hola geral 'buenos dias' "
+
 multiples_lineas = '''
     Hola Geral,
     te escribo esta carta
 '''
 salto_lineal = 'hola geral \nbuenos dias'
 edad_geral = '25'
+edad_nueva_geral = 25
 nac = 1998
 # FORMAS DE ANIDAR CADENAS DE TEXTO(STRING) CON MENSAJES
 mensaje_geral = 'Hola geral tienes ' + edad_geral + ' años'
-
 mensaje_format = 'Hola geral tienes {0} años y nacio en {1}'.format(edad_geral, nac)
+print("mensaje_format", mensaje_format)
 minuscula = 'geral'
 # format(0, 1,2,3,4,5)
 
@@ -87,18 +90,27 @@ x = True
 y = False
 string = 'dsdsds'
 nostring = ' '
-z = 2323232
+z = -1
 o = 0
-
+print("valores booleano", bool(o), bool(z))
 '''
     tipo de dato listado -> list
 '''
 
-listado = [2, 3, 4, 1, 10]
+listado = [2, "222", True, False, 10]
 listad_1 = list([2, 3, 4, 5, 10])
 
 print(type(listado))
-print(listado[2])
+print(listado[4])
+
+'''
+    tipo de dato diccionario -> dict
+'''
+dict_prueba = {
+    'nombre': 'geral',
+    'edad': '25'
+}
+print("dict_prueba: ", dict_prueba, type(dict_prueba), dict_prueba.get('edad'))
 
 if __name__ == "__main__":
    ...
