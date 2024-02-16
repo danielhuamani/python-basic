67cdf316715e80e75daf3e0fec6b19a51ec73006
daniel huamani <danielhuamani15@gmail.com>
2024-02-16 15:31:58 -0500

    agrego tarea de condicionales

---
+++ tareas/condicionales.py
@@ -0,0 +1,43 @@
+'''
+    tarea de condiciones, tiene como fin entender y practicar
+    los conceptos de if, else y elif ademas de los operadores
+    logicos y arimeticos
+'''
+
+'''
+    Ejercicio 1: Escribir un programa que pregunte al usuario 
+    su edad y muestre por pantalla si es mayor de edad o no.
+'''
+# Ejecutar el programa aca y mostrar en pantalla con print
+
+'''
+    Ejercicio 2: Escribir un programa que pida al usuario un 
+    número entero y muestre por pantalla si es par o impar.
+
+'''
+
+'''
+    Ejercicio 3: Escribir un programa que pida al usuario dos números y muestre por pantalla su división. Si el divisor es cero el
+    programa debe mostrar un error.
+'''
+
+# escribir programa aqui
+
+'''
+    Ejercicio 4: Escriba un programa que pida el año actual y
+    un año cualquiera y que escriba cuántos años han pasado desde ese año o
+    cuántos años faltan para llegar a ese año.
+'''
+
+'''
+    Ejercicio 5: Crea un programa que pida la nota (rango de 0 - 20)
+    de 3 trimestre y calcule la nota promedio .
+    El resultado que dará será desaprobado
+    si la media es menor de 11, aprobado si está entre 11 y 20.
+'''
+
+'''
+    Ejercicio 6: Este script pide al usuario que teclee una letra entre A, B, C, D. Si pulsa la letra A en
+    mayúsucla o en minúscula le dará el mensaje de que ha 
+    acertado, en caso contrario le dirá que se equivocó.
+'''
